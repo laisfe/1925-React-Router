d8f4d8aced8d6a07ce40cc841ac8f0f1d09aad5e
Lais Fernandes <laisffn@hotmail.com>
2022-06-13 15:24:19 -0300

    Style non existing route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./assets/css/base/base.css";
 import Home from "./paginas/Home";
+import Pagina404 from "./paginas/Pagina404";
 import Sobre from "./paginas/Sobre";
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
         </Route>
         <Route path="/sobre">
           <Sobre />
+        </Route>
+        <Route>
+          <Pagina404 />
         </Route>
       </Switch>
     </BrowserRouter>
