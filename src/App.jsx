7c86f2073718a996f52d1430f7be6a0aa58ef7dd
Lais Fernandes <laisffn@hotmail.com>
2022-06-13 14:48:56 -0300

    Implements react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import './assets/css/base/base.css'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./assets/css/base/base.css";
+import Home from "./paginas/Home";
+import Sobre from "./paginas/Sobre";
 
 function App() {
   return (
-  <>
-
-  </>
-  )
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/sobre">
+          <Sobre />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
