175a4741169da93098ca1b22a863f48b4c743a4f
Lais Fernandes <laisffn@hotmail.com>
2022-06-14 21:22:15 -0300

    Configure project to not reload when switching routes

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import imagem from "../assets/img/doguito.svg";
 import "../assets/css/componentes/cabecalho.css";
+import { Link } from "react-router-dom";
 
 const Cabecalho = () => {
   return (
@@ -9,32 +10,32 @@ const Cabecalho = () => {
         <span className="menu-hamburguer__icone"></span>
       </div>
       <div className="cabecalho-container">
-        <a className="flex flex--centro" href="/">
+        <Link className="flex flex--centro" to="/">
           <img className="cabecalho__logo" src={imagem} alt="Logo Doguito" />
           <h1 className="cabecalho__titulo">PetShop</h1>
-        </a>
+        </Link>
       </div>
       <nav className="manu-cabecalho">
         <ul className="menu-itens">
           <li>
-            <a className="menu-item menu-item--entrar" href="#">
+            <Link className="menu-item menu-item--entrar" to="#">
               Entrar
-            </a>
+            </Link>
           </li>
           <li>
-            <a className="menu-item" href="#">
+            <Link className="menu-item" to="#">
               Produtos
-            </a>
+            </Link>
           </li>
           <li>
-            <a className="menu-item" href="/">
+            <Link className="menu-item" to="/">
               Blog
-            </a>
+            </Link>
           </li>
           <li>
-            <a className="menu-item" href="/sobre">
+            <Link className="menu-item" to="/sobre">
               Sobre
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
